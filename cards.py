494e3a79129e6c572e7dc11650ac386ab329ae94
Ruslan <rus.babaef@yandex.ru>
2021-12-20 23:24:03 +0300

    cards_info_2

--- cards.py
+++ cards.py
@@ -3,17 +3,33 @@ import sqlite3
 import os
 
 database = os.path.join('data', 'card_db.db')
-PLAYCARDS_DATA = sqlite3.connect(database).cursor().execute("""SELECT * FROM playcards""")
-BONUSCARDS_DATA = sqlite3.connect(database).cursor().execute("""SELECT * FROM bonuscards""")
+PLAYCARDS_DATA = sqlite3.connect(database).cursor().execute("""SELECT fraction, name, specialization, 
+                                        pace, chakra, resistance, health, technic, synergy FROM playcards""")
+BONUSCARDS_DATA = sqlite3.connect(database).cursor().execute("""SELECT fraction, name FROM bonuscards""")
 PLAYCARDS_DATA, BONUSCARDS_DATA = [list(i) for i in PLAYCARDS_DATA], [list(i) for i in BONUSCARDS_DATA]
+p_images = ['shu.jpg', 'pashke.jpg', 'akemi.jpg', 'raik.jpg', 'kentaru.jpg', 'hiruko.jpg',
+            'keiko.jpg', 'akito.jpg', 'ryu.jpg', 'kitsu.jpg', 'benkei.jpg', 'teeru.jpg']
+b_images = ['bar.jpg', 'himera.jpg', 'tsunami.jpg', 'king_mouse.jpg', 'ren.jpg',
+            'hymn.jpg', 'turtle.jpg', 'kin.jpg', 'true_medic.jpg', 'ambitions.jpg']
+PLAYCARDS, BONUSCARDS = [], []
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('resources', 'cards')
+    fullname = os.path.join(fullname, name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
 
 
 class PlayCard(pygame.sprite.Sprite):
 
     def __init__(self, image, fraction, name, spec, pace, chakra,
-                 resist, health, technic, technic_info, passive_ability_info, synergy):
+                 resist, health, technic, synergy):
         super().__init__()
-        self.image = image
+        self.image = load_image(image)
         self.fraction = fraction
         self.name = name
         self.spec = spec
@@ -22,9 +38,8 @@ class PlayCard(pygame.sprite.Sprite):
         self.resist = resist
         self.health = health
         self.technic = technic.split()
-        self.technic_info = technic_info
-        self.passive_ability_info = passive_ability_info
         self.synergy = synergy.split()
+        self.rect = self.image.get_rect()
 
 
     def update(self, *args):
@@ -36,14 +51,33 @@ class PlayCard(pygame.sprite.Sprite):
     def get_info(self):
         pass
 
+    def __str__(self):
+        return self.name
+
 
 class BonusCard(pygame.sprite.Sprite):
 
-    def __init__(self, image, fraction, name, ability_info):
+    def __init__(self, image, fraction, name):
         super().__init__()
+        self.image = load_image(image)
+        self.fraction = fraction
+        self.name = name
+        self.rect = self.image.get_rect()
 
     def ability(self):
         pass
 
     def get_info(self):
         pass
+
+    def __str__(self):
+        return self.name
+
+
+for i in range(len(PLAYCARDS_DATA)):
+    data = PLAYCARDS_DATA[i]
+    PLAYCARDS.append(PlayCard(p_images[i], data[0], data[1], data[2], data[3], data[4],
+                              data[5], data[6], data[7], data[8]))
+for i in range(len(BONUSCARDS_DATA)):
+    data = BONUSCARDS_DATA[i]
+    BONUSCARDS.append(BonusCard(b_images[i], data[0], data[1]))
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sys
-import pygame
 import os
+import pygame
+import cards
+from cards import PLAYCARDS, BONUSCARDS
 
-
-FRACTION = 0
-KONOHAGAKURE = 0
-IVAGAKURE = 1
+FRACTION = None
+KONOHAGAKURE = 'KONOHAGAKURE'
+IVAGAKURE = 'IVAGAKURE'
 FPS = 60
 
 
@@ -17,18 +18,37 @@ def load_image(*names, colorkey=None):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
     image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
     return image
 
 
-def load_cardlist(cards):
-    pass
+def load_cardlist(surf, group, cards):
+    sprites = group.sprites()
+    group.empty()
+    sprites[0].image, sprites[1].image = pygame.transform.scale(sprites[0].image, (183, 95)), \
+                                         pygame.transform.scale(sprites[1].image, (183, 95))
+    sprites[0].rect, sprites[1].rect = sprites[0].image.get_rect(), sprites[1].image.get_rect()
+    sprites[0].rect.x, sprites[0].rect.y = 49, height - 110
+    sprites[1].rect.x, sprites[1].rect.y = 248, height - 110
+    group.add(sprites[0])
+    group.add(sprites[1])
+    exit_button = pygame.Rect(width - 70, 15, 50, 50)
+    pygame.draw.rect(surf, pygame.Color('black'), exit_button, 3)
+    pygame.draw.line(surf, pygame.Color('black'), (width - 60, 25), (width - 30, 55), 5)
+    pygame.draw.line(surf, pygame.Color('black'), (width - 30, 25), (width - 60, 55), 5)
+    if cards:
+        for i in range(len(BONUSCARDS)):
+            if BONUSCARDS[i].fraction == FRACTION:
+                BONUSCARDS[i].image = pygame.transform.scale(BONUSCARDS[i].image, (150, 225))
+                BONUSCARDS[i].rect.x = 5 + 160 * ((i % 5) % 3)
+                BONUSCARDS[i].rect.y = 200 + 275 * ((i % 5) // 3)
+                group.add(BONUSCARDS[i])
+    else:
+        for i in range(len(PLAYCARDS)):
+            if PLAYCARDS[i].fraction == FRACTION:
+                PLAYCARDS[i].image = pygame.transform.scale(PLAYCARDS[i].image, (150, 225))
+                PLAYCARDS[i].rect.x = 5 + 160 * ((i % 6) % 3)
+                PLAYCARDS[i].rect.y = 200 + 275 * ((i % 6) // 3)
+                group.add(PLAYCARDS[i])
 
 
 def terminate():
@@ -50,12 +70,12 @@ def start_screen():
                 if event.key == pygame.K_ESCAPE:
                     terminate()
                 elif event.key == pygame.K_SPACE:
-                    pygame.time.delay(150)
+                    pygame.time.wait(150)
                     return choose_fraction()
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == pygame.BUTTON_LEFT:
                     if play_button.collidepoint(event.pos):
-                        pygame.time.delay(150)
+                        pygame.time.wait(150)
                         return choose_fraction()
         pygame.display.flip()
 
@@ -65,7 +85,8 @@ def choose_fraction():
     fon = load_image('back_and_buttons', 'cf_background.jpg')
     konohagakure, ivagakure = load_image('back_and_buttons', 'konoha_background.jpg'), \
                               load_image('back_and_buttons', 'iva_background.jpg')
-    info_1, info_2 = load_image('back_and_buttons', 'info_1.png'), load_image('back_and_buttons', 'info_2.png')
+    info_1, info_2 = load_image('back_and_buttons', 'info_1.png'), \
+                     load_image('back_and_buttons', 'info_2.png')
     k_rect, i_rect = konohagakure.get_rect(), ivagakure.get_rect()
     inf_rect1, inf_rect2 = info_1.get_rect(), info_2.get_rect()
     k_rect.x, k_rect.y = width - 390, 0
@@ -88,65 +109,69 @@ def choose_fraction():
                 if event.button == pygame.BUTTON_LEFT:
                     if k_rect.collidepoint(event.pos):
                         FRACTION = KONOHAGAKURE
-                        pygame.time.delay(150)
+                        pygame.time.wait(150)
                         return
                     elif i_rect.collidepoint(event.pos):
                         FRACTION = IVAGAKURE
-                        pygame.time.delay(150)
+                        pygame.time.wait(150)
                         return
                     elif inf_rect1.collidepoint(event.pos):
                         FRACTION = KONOHAGAKURE
-                        pygame.time.delay(150)
+                        pygame.time.wait(150)
                         return cards_info()
                     elif inf_rect2.collidepoint(event.pos):
                         FRACTION = IVAGAKURE
-                        pygame.time.delay(150)
+                        pygame.time.wait(150)
                         return cards_info()
         pygame.display.flip()
 
 
 def cards_info():
+    info_sprites = pygame.sprite.Group()
     fon = load_image('back_and_buttons', 'basic_background.jpg')
     screen.blit(fon, (0, 0))
     exit_button = pygame.Rect(width - 70, 15, 50, 50)
     pygame.draw.rect(screen, pygame.Color('black'), exit_button, 3)
     pygame.draw.line(screen, pygame.Color('black'), (width - 60, 25), (width - 30, 55), 5)
     pygame.draw.line(screen, pygame.Color('black'), (width - 30, 25), (width - 60, 55), 5)
-    player_cards, bonus_cards = load_image('back_and_buttons', 'player_cards.png'), \
-                                load_image('back_and_buttons', 'bonus_cards.png')
-    pc_rect, bc_rect = player_cards.get_rect(), bonus_cards.get_rect()
-    pc_rect.x, pc_rect.y = 102, 285
-    bc_rect.x, bc_rect.y = 102, 500
-    screen.blit(player_cards, (pc_rect.x, pc_rect.y))
-    screen.blit(bonus_cards, (bc_rect.x, bc_rect.y))
+    playcards, bonuscards = pygame.sprite.Sprite(), pygame.sprite.Sprite()
+    playcards.image, bonuscards.image = load_image('back_and_buttons', 'playcards.png'), \
+                                        load_image('back_and_buttons', 'bonuscards.png')
+    playcards.rect, bonuscards.rect = playcards.image.get_rect(), bonuscards.image.get_rect()
+    playcards.rect.x, playcards.rect.y = 102, 285
+    bonuscards.rect.x, bonuscards.rect.y = 102, 500
+    info_sprites.add(playcards)
+    info_sprites.add(bonuscards)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE or event.key == pygame.K_BACKSPACE:
-                    pygame.time.delay(150)
+                    pygame.time.wait(150)
                     return choose_fraction()
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == pygame.BUTTON_LEFT:
                     if exit_button.collidepoint(event.pos):
-                        pygame.time.delay(150)
+                        pygame.time.wait(150)
                         return choose_fraction()
-                    elif pc_rect.collidepoint(event.pos):
-                        pygame.time.delay(150)
-                        load_cardlist(0)
-                    elif bc_rect.collidepoint(event.pos):
-                        pygame.time.delay(150)
-                        load_cardlist(1)
+                    elif playcards.rect.collidepoint(event.pos):
+                        pygame.time.wait(150)
+                        screen2 = pygame.Surface(screen.get_size())
+                        screen2.blit(fon, (0, 0))
+                        load_cardlist(screen2, info_sprites, 0)
+                        screen.blit(screen2, (0, 0))
+                    elif bonuscards.rect.collidepoint(event.pos):
+                        pygame.time.wait(150)
+                        screen2 = pygame.Surface(screen.get_size())
+                        screen2.blit(fon, (0, 0))
+                        load_cardlist(screen2, info_sprites, 1)
+                        screen.blit(screen2, (0, 0))
+        info_sprites.draw(screen)
         pygame.display.flip()
 
 
-if __name__ == '__main__':
-    pygame.init()
-    size = width, height = 480, 854
-    pygame.display.set_caption('SWAY')
-    screen = pygame.display.set_mode(size)
-    clock = pygame.time.Clock()
+def main():
     start_screen()
     screen.blit(load_image('back_and_buttons', 'main_battlefield.jpg'), (0, 0))
     while True:
@@ -155,3 +180,13 @@ if __name__ == '__main__':
                 terminate()
         clock.tick(FPS)
         pygame.display.flip()
+    return start_screen()
+
+
+if __name__ == '__main__':
+    pygame.init()
+    size = width, height = 480, 854
+    pygame.display.set_caption('SWAY')
+    screen = pygame.display.set_mode(size)
+    clock = pygame.time.Clock()
+    main()
